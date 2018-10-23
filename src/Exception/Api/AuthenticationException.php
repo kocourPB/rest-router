216550ce151505d87075bdc386573f8d1488db2a
Michal Lokšík <mloksik@gmail.com>
2018-10-23 11:59:32 +0200

    fixed typo

--- src/Exception/Api/AuthenticationException.php
+++ src/Exception/Api/AuthenticationException.php
@@ -12,7 +12,7 @@ class AuthenticationException extends ApiException
 
 	public const MISSING_HEADER_VALUE = 801;
 
-	public const BAD_AUTHANTICATION = 802;
+	public const BAD_AUTHENTICATION = 802;
 
 
 	public function __construct(string $message = '', int $errorCode, array $errors = [])
